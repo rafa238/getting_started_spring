52226f62a4de6f11aa760196dd411ce82fa05e8e
rafa238 <rafalaureano246@gmail.com>
2023-05-10 16:40:55 -0600

    Parameters example

--- spring-boot-started/src/main/java/com/rafa238/springboot/web/app/controllers/ParamsController.java
+++ spring-boot-started/src/main/java/com/rafa238/springboot/web/app/controllers/ParamsController.java
@@ -12,11 +12,22 @@ public class ParamsController {
 	
 	/*
 	 * We can receive parameters in a url way (?example=example+String)
-	 * @RequestParam can especifying if the current param is required, or default Value
+	 * @RequestParam can specifying if the current param is required, or default Value
 	 */
 	@GetMapping("/string")
 	public String param(Model model, @RequestParam(name = "param") String parameter) {
 		model.addAttribute("result", "The string sent is " + parameter );
 		return "params/watch";
 	}
+	
+	/*
+	 * This is an example how we can get multiple parameters
+	 * If we omit the @RequesParam name, we consider variable's name as parameter name
+	 */
+	@GetMapping("/mix-params")
+	public String mixParams(Model model, @RequestParam String greeting, @RequestParam Integer num) {
+		model.addAttribute("result", "The greetiong sent is: " + greeting + ", and number: " + num );
+		return "params/watch";
+	}
+	
 }
